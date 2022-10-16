0f9f1a90bd6f0d80333111d241cfff83df9e09af
SeYeongYoon <ssyoun4092@naver.com>
2022-10-16 12:10:38 +0900

    [추가] Test용 MockRealmModel 추가

---
+++ BlackCatSDK/BlackCatSDKTests/CatSDKRealmTests/MockModel.swift
@@ -0,0 +1,22 @@
+//
+//  MockModel.swift
+//  BlackCatSDKTests
+//
+//  Created by SeYeong on 2022/10/16.
+//
+
+import Foundation
+
+import RealmSwift
+
+class MockRealmModel2: Object {
+    @Persisted(primaryKey: true) var id: String = UUID().uuidString
+    @Persisted var createdAt: Date = Date()
+    @Persisted var content: String
+
+    convenience init(content: String) {
+        self.init()
+
+        self.content = content
+    }
+}
