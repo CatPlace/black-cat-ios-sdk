3d69a10499f697210a34bbb4684d83f814d80e5d
SeYeongYoon <ssyoun4092@naver.com>
2022-12-14 18:51:02 +0900

    [추가] PostTattooAPI MultipartForm 구현

--- BlackCatSDK/BlackCatSDK/CatSDKNetwork/API/TattooAPIs/PostTattooAPI.swift
+++ BlackCatSDK/BlackCatSDK/CatSDKNetwork/API/TattooAPIs/PostTattooAPI.swift
@@ -5,20 +5,26 @@
 //  Created by SeYeong on 2022/11/09.
 //
 
-import UIKit
+import Foundation
 
 import Moya
 
 struct PostTattooAPI: ServiceAPI {
     typealias Response = DTO.Tattoo.Post
 
-    var tattooImages: [UIImage]
-    var tattooInfos: [TattooInfo]
+    var tattooImageDatas: [Data]
+    var tattooInfo: TattooInfo
     var path: String = "tattoos"
     var method: Moya.Method { .post }
     var multiPartFormDatas: [MultipartFormData] {
         // TODO: - 멀티파트 폼 해결
-        []
+        let tattooInfoData = try! JSONEncoder().encode(tattooInfo)
+        var formDatas: [MultipartFormData] = [.init(provider: .data(tattooInfoData), name: "tattooInfo")]
+        tattooImageDatas.forEach { imageData in
+            formDatas.append(.init(provider: .data(imageData), name: "images"))
+        }
+
+        return formDatas
     }
     var task: Moya.Task {
         .uploadMultipart(multiPartFormDatas)
